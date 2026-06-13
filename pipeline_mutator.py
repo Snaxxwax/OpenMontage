5a5a4a6d9583ccbc6a99ae5a491767379ca6d694
snaxxwax <errlybird49@gmail.com>
2026-06-13 01:02:15 -0500

    optimization: reduced SDS score to 0.046554

--- pipeline_mutator.py
+++ pipeline_mutator.py
@@ -7,7 +7,7 @@ Dissimilarity Score (SDS); lower is better.
 
 # Visual structure controls. Keep values numeric and JSON-serializable.
 MOTION_DENSITY = 0.58
-CUT_RHYTHM = 0.52
+CUT_RHYTHM = 0.61
 EVIDENCE_CARD_SCALE = 0.44
 TYPOGRAPHY_WEIGHT = 0.62
 CONTRAST_BALANCE = 0.68
